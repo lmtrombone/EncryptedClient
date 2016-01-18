bf0a61014fbdfab72f1f76b85ae024294fb74931
2yangk23 <2yangk23@gmail.com>
2016-01-17 19:45:30 -0800

    Implemented S3 upload and download (needs encryption)

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -5,20 +5,74 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
+import java.io.*;
 
 public class FileUtils
 {
-    private static final String BUCKET = "temp-ucsb-bucket";
-    private static final String KEY = "tempkey";
+    private static final String BUCKET = "ucsb-temp-bucket-name";
+    private static final int BUFFER_SIZE = 1024 * 10; // 10Kb
 
-    public static void uploadFile(File file) {
+    public static void uploadFile(File file, String id) {
+        AmazonS3 s3 = getClient();
+
+        // Upload the files (no encryption)
+        //s3.putObject(BUCKET, id, file);
+
+        // Encrypted method
+        PipedInputStream in = new PipedInputStream();
+        try {
+            OutputStream out = new PipedOutputStream(in);
+            InputStream reader = new BufferedInputStream(new FileInputStream(file));
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            int bytesRead;
+            while ((bytesRead = reader.read(buffer)) > -1) {
+                // TODO: encrypt buffer
+                out.write(buffer, 0, bytesRead);
+            }
+            out.flush();
+            out.close();
+            reader.close();
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(in.available());
+            s3.putObject(BUCKET, id, in, metadata);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void downloadFile(String path, String id) {
+        AmazonS3 s3 = getClient();
+
+        GetObjectRequest objReq = new GetObjectRequest(BUCKET, id);
+        File file = new File(path);
+
+        // No encryption method
+        //s3.getObject(objReq, file);
+
+        // Encrypted method
+        InputStream in = s3.getObject(objReq).getObjectContent();
+        try {
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) > -1) {
+                // TODO: decrypt buffer
+                out.write(buffer, 0, bytesRead);
+            }
+            out.flush();
+            out.close();
+            in.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static AmazonS3 getClient() {
         // Load AWS Credentials (This needs to be setup on each computer)
         AWSCredentials credentials;
         try {
@@ -35,19 +89,12 @@ public class FileUtils
         Region usWest2 = Region.getRegion(Regions.US_WEST_2);
         s3.setRegion(usWest2);
 
-        // Upload the files
-        try {
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            InputStream stream = new ByteArrayInputStream(bytes);
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(bytes.length);
-            s3.putObject(BUCKET, KEY, stream, metadata);
-        } catch (IOException ex) {
-            System.out.println("ERROR: " + ex);
-        }
+        return s3;
     }
 
     public static void main(String[] args) {
-
+        File f = new File("C:\\Users\\Computer\\Documents\\txt\\test.txt");
+        uploadFile(f, "test.txt");
+        downloadFile("C:\\Users\\Computer\\Documents\\txt\\testdown.txt", "test.txt");
     }
 }
